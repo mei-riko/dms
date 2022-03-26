26a744301fb740696e369c0083df507b47a11146
mei-riko <dobrovolska.marina@gmail.com>
2022-03-26 23:22:59 +0300

    theme dark start

---
+++ src/js/mouseup.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+$(document).on('mouseup', function (e){
+    // let $calculatorModal = $(".calculator .calculator__modal.calculator__modal--active");
+    // if (!$calculatorModal.is(e.target) // клик был не по блоку
+    //       && $calculatorModal.has(e.target).length === 0 // и не по его дочерним 
+    // ) {
+    //     $calculatorModal.removeClass('calculator__modal--active');
+    //     $calculatorModal.slideUp();
+    // }
+});
